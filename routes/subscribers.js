121208b9355f4fdb8e15b54da2de75394fd79579
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-09-17 17:28:57 +0530

    final commit

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -33,16 +33,45 @@ router.post('/', async (req, res) => {
     }
   })
 
-// updating one
-router.patch('/', (req,res) => {
-
-})
-
-// deleting one
-router.delete('/:id', (req,res) => {
-
-})
-
-
-
-module.exports = router
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if (req.body.name != null) {
+      res.subscriber.name = req.body.name
+    }
+    if (req.body.subscribedToChannel != null) {
+      res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try {
+      const updatedSubscriber = await res.subscriber.save()
+      res.json(updatedSubscriber)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+  
+  // Deleting One
+  router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+      await res.subscriber.remove()
+      res.json({ message: 'Deleted Subscriber' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  })
+  
+  async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+      subscriber = await Subscriber.findById(req.params.id)
+      if (subscriber == null) {
+        return res.status(404).json({ message: 'Cannot find subscriber' })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.subscriber = subscriber
+    next()
+  }
+  
+  module.exports = router
